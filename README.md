d8b6e19306e5bdeb2e64bcf0f695a2c1cc658f73
starry99 <lucky112@kaist.ac.kr>
2023-07-28 02:50:31 +0900

    remove ssl

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # LX-KAIST Web demo
+
+```
+$ npm i
+
+$ gatsby develop
+```
+
+gatsby cloud와 연동되어있음 이 repository에 push하면 자동으로 반영
--- src/components/mcMap.js
+++ src/components/mcMap.js
@@ -3,15 +3,15 @@ import { Button } from "@material-tailwind/react"
 
 const McMap = () => (
   <div className="w-full h-full relative bg-black">
-        <div className="absolute top-20 left-1/2 transform -translate-x-1/2 flex justify-center z-[1200]">
-        <Button size="lg" className="cursor-default">
+        <div className="absolute top-20 left-1/2 transform -translate-x-1/2 flex justify-center">
+        <Button size="lg" className="cursor-default" onClick={()=> window.open("http://152.70.247.28/webmap", "_blank")} >
         <p className="text-xl">공간정보 데이터 시각화</p>
         <p className="text-sm text-gray-300">마인크래프트 2.5D 지도</p>
       </Button>
     </div>
     <iframe
       id="map"
-      src="https://daejeon.n-e.kr/webmap/"
+      src="http://152.70.247.28/webmap/"
       className="h-full w-full -mt-16"
       title="map"
     />
